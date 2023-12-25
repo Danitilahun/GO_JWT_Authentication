313cd13fff64aa8123371bced82bd64c6e66320c
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-26 01:13:45 +0300

    Adding hash password function

--- controller/userController.go
+++ controller/userController.go
@@ -2,9 +2,17 @@ package controller
 
 import (
 	"github.com/Danitilahun/GO_JWT_Authentication.git/database"
-	"github.com/Danitilahun/GO_JWT_Authentication.git/helper"
-	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
+	"golang.org/x/crypto/bcrypt"
+	"log"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "auth", "user")
+
+func HashPassword(password string) string {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	if err != nil {
+		log.Panic(err)
+	}
+	return string(bytes)
+}
