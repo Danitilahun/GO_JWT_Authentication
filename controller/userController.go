fcb07783945dcf9e84447b21632e6edd9c4ecdbb
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-26 01:25:36 +0300

    Jwt token generate function added

--- controller/userController.go
+++ controller/userController.go
@@ -1,15 +1,25 @@
 package controller
 
 import (
+	"context"
 	"fmt"
 	"log"
+	"net/http"
+	"time"
 
 	"github.com/Danitilahun/GO_JWT_Authentication.git/database"
+	helper "github.com/Danitilahun/GO_JWT_Authentication.git/helper"
+	"github.com/Danitilahun/GO_JWT_Authentication.git/model"
+	"github.com/gin-gonic/gin"
+	"github.com/go-playground/validator"
+	"go.mongodb.org/mongo-driver/bson/primitive"
 	"go.mongodb.org/mongo-driver/mongo"
 	"golang.org/x/crypto/bcrypt"
+	"gopkg.in/mgo.v2/bson"
 )
 
 var userCollection *mongo.Collection = database.OpenCollection(database.Client, "auth", "user")
+var validate = validator.New()
 
 func HashPassword(password string) string {
 	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
--- helper/JwtHelper.go
+++ helper/JwtHelper.go
@@ -2,6 +2,7 @@ package helper
 
 import (
 	"fmt"
+	"log"
 	"os"
 	"time"
 
@@ -21,6 +22,40 @@ type SignedDetails struct {
 }
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	claims := &SignedDetails{
+		Email:      email,
+		First_name: firstName,
+		Last_name:  lastName,
+		Uid:        uid,
+		User_type:  userType,
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
+		},
+	}
+
+	refreshClaims := &SignedDetails{
+		StandardClaims: jwt.StandardClaims{
+			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
+		},
+	}
+
+	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
+
+	if err != nil {
+		log.Panic(err)
+		return
+	}
+
+	return token, refreshToken, err
+}
 
 func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
 
