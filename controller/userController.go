4654ad75eda924114820f9222e326df0481d0050
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-25 21:54:12 +0300

    Adding get token from header and check emptyness function

--- controller/userController.go
+++ controller/userController.go
@@ -2,6 +2,8 @@ package controller
 
 import (
 	"github.com/Danitilahun/GO_JWT_Authentication.git/database"
+	"github.com/Danitilahun/GO_JWT_Authentication.git/helper"
+	"github.com/gin-gonic/gin"
 	"go.mongodb.org/mongo-driver/mongo"
 )
 
--- helper/JwtHelper.go
+++ helper/JwtHelper.go
@@ -1,1 +1,24 @@
 package helper
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func getTokenFromHeader(c *gin.Context) string {
+	// Extract token from the request header
+	clientToken := c.Request.Header.Get("token")
+
+	// Check if the token is empty
+	if clientToken == "" {
+		// Respond with an error indicating missing authorization header
+		c.JSON(http.StatusInternalServerError, gin.H{"error": "No Authorization header provided"})
+
+		// Abort further processing
+		c.Abort()
+		return ""
+	}
+
+	return clientToken
+}
