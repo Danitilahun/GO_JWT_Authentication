8481f527bd93cd1af2f75c20a2d005a3f2b4153c
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-25 21:22:16 +0300

    Creating check user type function as helper function

---
+++ helper/authHelper.go
@@ -0,0 +1,16 @@
+package helper
+
+import (
+	"errors"
+	"github.com/gin-gonic/gin"
+)
+
+func CheckUserType(c *gin.Context, role string) (err error) {
+	userType := c.GetString("user_type")
+	err = nil
+	if userType != role {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	return err
+}
