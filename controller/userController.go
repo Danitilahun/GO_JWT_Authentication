6972baa700e9dcaef57bed557705e7f2a94cd078
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-26 02:09:38 +0300

    get indivitual user function

--- controller/userController.go
+++ controller/userController.go
@@ -239,3 +239,24 @@ func GetUsers() gin.HandlerFunc {
 		c.JSON(http.StatusOK, allUsers[0])
 	}
 }
+
+func GetUser() gin.HandlerFunc {
+	return func(c *gin.Context) {
+		userId := c.Param("user_id")
+
+		if err := helper.MatchUserTypeToUid(c, userId); err != nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": err.Error()})
+			return
+		}
+		var ctx, cancel = context.WithTimeout(context.Background(), 100*time.Second)
+
+		var user models.User
+		err := userCollection.FindOne(ctx, bson.M{"user_id": userId}).Decode(&user)
+		defer cancel()
+		if err != nil {
+			c.JSON(http.StatusInternalServerError, gin.H{"error": err.Error()})
+			return
+		}
+		c.JSON(http.StatusOK, user)
+	}
+}
