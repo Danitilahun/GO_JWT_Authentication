55d61419ad923a87172422a39d2eb9dcfb7c6ee5
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-26 01:27:46 +0300

    Add comment

--- helper/JwtHelper.go
+++ helper/JwtHelper.go
@@ -23,7 +23,24 @@ type SignedDetails struct {
 
 var SECRET_KEY string = os.Getenv("SECRET_KEY")
 
+// GenerateAllTokens generates JWT (JSON Web Token) and Refresh Token pair based on the provided user details.
+// It creates a signed JWT containing user-specific claims (such as email, first name, last name, user type, UID)
+// and sets an expiration time for both the JWT and Refresh Token.
+// Parameters:
+//
+//	email: The email address of the user.
+//	firstName: The first name of the user.
+//	lastName: The last name of the user.
+//	userType: The type of user (e.g., admin, regular user).
+//	uid: The unique identifier for the user.
+//
+// Returns:
+//
+//	signedToken: The signed JWT representing the user's details with an expiration time of 24 hours.
+//	signedRefreshToken: The signed Refresh Token with an expiration time of 7 days (168 hours).
+//	err: Any error encountered during token generation.
 func GenerateAllTokens(email string, firstName string, lastName string, userType string, uid string) (signedToken string, signedRefreshToken string, err error) {
+	// Create JWT claims containing user-specific details and set expiration time for the access token
 	claims := &SignedDetails{
 		Email:      email,
 		First_name: firstName,
@@ -31,29 +48,37 @@ func GenerateAllTokens(email string, firstName string, lastName string, userType
 		Uid:        uid,
 		User_type:  userType,
 		StandardClaims: jwt.StandardClaims{
+			// Set expiration time for 24 hours from the current time
 			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(24)).Unix(),
 		},
 	}
 
+	// Create claims for Refresh Token and set expiration time for 7 days
 	refreshClaims := &SignedDetails{
 		StandardClaims: jwt.StandardClaims{
+			// Set expiration time for 7 days from the current time
 			ExpiresAt: time.Now().Local().Add(time.Hour * time.Duration(168)).Unix(),
 		},
 	}
 
+	// Generate a signed JWT using HS256 signing method and the provided claims
 	token, err := jwt.NewWithClaims(jwt.SigningMethodHS256, claims).SignedString([]byte(SECRET_KEY))
 	if err != nil {
+		// Handle error if token generation fails
 		log.Panic(err)
 		return
 	}
 
+	// Generate a signed Refresh Token using HS256 signing method and the refreshClaims
 	refreshToken, err := jwt.NewWithClaims(jwt.SigningMethodHS256, refreshClaims).SignedString([]byte(SECRET_KEY))
 
 	if err != nil {
+		// Handle error if Refresh Token generation fails
 		log.Panic(err)
 		return
 	}
 
+	// Return the generated token pair and any error encountered (if applicable)
 	return token, refreshToken, err
 }
 
