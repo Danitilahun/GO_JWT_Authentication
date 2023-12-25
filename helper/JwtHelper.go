d9ac6dab4d1ee8c16e719ae5ea23711a95734e38
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-25 23:35:03 +0300

    Adding validate token

--- helper/JwtHelper.go
+++ helper/JwtHelper.go
@@ -1,6 +1,12 @@
 package helper
 
-import "github.com/dgrijalva/jwt-go"
+import (
+	"fmt"
+	"os"
+	"time"
+
+	"github.com/dgrijalva/jwt-go"
+)
 
 // SignedDetails represents a structure combining user-specific details and standard JWT claims.
 // It includes fields for Email, First_name, Last_name, Uid, and User_type
@@ -12,4 +18,36 @@ type SignedDetails struct {
 	Uid        string
 	User_type  string
 	jwt.StandardClaims
+}
+
+var SECRET_KEY string = os.Getenv("SECRET_KEY")
+
+func ValidateToken(signedToken string) (claims *SignedDetails, msg string) {
+	token, err := jwt.ParseWithClaims(
+		signedToken,
+		&SignedDetails{},
+		func(token *jwt.Token) (interface{}, error) {
+			return []byte(SECRET_KEY), nil
+		},
+	)
+
+	if err != nil {
+		msg = err.Error()
+		return
+	}
+
+	claims, ok := token.Claims.(*SignedDetails)
+	if !ok {
+		msg = fmt.Sprintf("the token is invalid")
+		msg = err.Error()
+		return
+	}
+
+	// Unix returns t as a Unix time, the number of seconds elapsed since January 1, 1970 UTC.
+	if claims.ExpiresAt < time.Now().Local().Unix() {
+		msg = fmt.Sprintf("token is expired")
+		msg = err.Error()
+		return
+	}
+	return claims, msg
 }
