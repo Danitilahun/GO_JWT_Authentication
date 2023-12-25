1d8b1e999936e63a91815e573e2192c3e5931972
Daniel Tilahun <tiledan2015@gmail.com>
2023-12-25 21:25:34 +0300

    create a function for matching user information like user type and uid

--- helper/authHelper.go
+++ helper/authHelper.go
@@ -14,3 +14,16 @@ func CheckUserType(c *gin.Context, role string) (err error) {
 	}
 	return err
 }
+
+func MatchUserTypeToUid(c *gin.Context, userId string) (err error) {
+	userType := c.GetString("user_type")
+	uid := c.GetString("uid")
+	err = nil
+
+	if userType == "USER" && uid != userId {
+		err = errors.New("Unauthorized to access this resource")
+		return err
+	}
+	err = CheckUserType(c, userType)
+	return err
+}
